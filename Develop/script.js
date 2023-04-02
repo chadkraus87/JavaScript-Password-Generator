c53011115321846b959bd2ba7f9da003c9678853
chadkraus87 <127354484+chadkraus87@users.noreply.github.com>
2023-04-02 13:39:18 -0500

    Added vars to JavaScript, statements. Added and completed README.

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,32 @@
 // Assignment code here
+var lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz"
+var uppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var numericCharacters = "0123456789"
+var specialCharacters = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 
+// Prompt the user for their password criteria and generate a password.
+function generatePassword() {
+  var passwordLength = parseInt(
+    prompt("Enter the length of your desired password. Note: Your password must be at least 8 characters long and no more than 128.")
+  );
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+  // Check for a valid password length.
+  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    alert("Invalid password length. Please try again.");
+    return;
+  }
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  // Prompt the user for character types to include in their generated password.
+  var includeLowercase = confirm("Would you like your password to have lowercase letters?");
+  var includeUppercase = confirm("Would you like your password to have uppercase letters?");
+  var includeNumeric = confirm("Would you like your password to have numbers?");
+  var includeSpecial = confirm("Would you like your password to have special characters?");
 
-  passwordText.value = password;
-
-}
+  // Check to see if at least one character type has been selected.
+  if (!includeLowercase && !includeUppercase && !includeNumeric && !includeSpecial) {
+    alert("Please select at least one character type and try again.");
+    return;
+  }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # JavaScript-Password-Generator
+
+## Description
+
 A password generator created using JavaScript.
+
+[Deployed App](https://chadkraus87.github.io/JavaScript-Password-Generator/)
+
+## Installation
+
+No installation is necessary for this application.
+
+## Usage
+
+This webpage is an example of a Password Generator with prompts created using JavaScript.
+
+## Credits
+
+[JavaScript Statements](https://www.w3schools.com/js/js_statements.asp)
+
+[JavaScript if...else Statements](https://www.w3schools.com/jsref/jsref_if.asp)
+
+[JavaScript var Statement](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/var)
+
+[Professional README Guide](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
+
+[JavaScript function declaration Statement](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/function)
+
+[JavaScript for Statement](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for)
+
+[JavaScript Expressions and Operators](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators)
+
+[JavaScript this Statement](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/this)
+
+
+## License
+
+MIT License
+
+A short and simple permissive license with conditions only requiring preservation of copyright and license notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code.
